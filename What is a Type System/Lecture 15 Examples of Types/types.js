6cc4db8e906cb6206cb5c6fe2a58032025e2c5f0
MJunaidK <junaidcts@gmail.com>
2023-04-15 16:18:44 -0400

    Type system

---
+++ What is a Type System/Lecture 15 Examples of Types/types.js
@@ -0,0 +1,15 @@
+var today = new Date();
+// shortcut to say here are the different properties and methods that this value right here has.
+today.getDate();
+//today.kshdkjhadk
+var person = {
+    age: 20,
+};
+console.log(person.age);
+var Color = /** @class */ (function () {
+    function Color() {
+    }
+    return Color;
+}());
+var red = new Color();
+//red.jabjdbff
---
+++ What is a Type System/Lecture 15 Examples of Types/types.ts
@@ -0,0 +1,15 @@
+const today = new Date();
+// shortcut to say here are the different properties and methods that this value right here has.
+today.getDate();
+//today.kshdkjhadk
+
+const person = {
+  age: 20,
+};
+
+console.log(person.age);
+
+class Color {}
+
+const red = new Color();
+//red.jabjdbff
