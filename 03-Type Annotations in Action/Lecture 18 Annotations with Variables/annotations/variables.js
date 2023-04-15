d2dac78349be1b702a12049f4f9956745ec0a63b
MJunaidK <junaidcts@gmail.com>
2023-04-15 17:16:57 -0400

    Types annotation

---
+++ 03-Type Annotations in Action/Lecture 18 Annotations with Variables/annotations/variables.js
@@ -0,0 +1,9 @@
+var apples = 5;
+apples = 10;
+var speed = "fast";
+var hasName = true;
+var nothingMuch = null;
+var nothing = undefined;
+// built in objects
+var now = new Date();
+console.log(now);
