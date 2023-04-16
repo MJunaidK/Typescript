3eb6e601c73db486536097868bdebc6a373f6db6
MJunaidK <junaidcts@gmail.com>
2023-04-16 13:49:16 -0400

    Annotation for anonymous functions

---
+++ 03-Type Annotations in Action/Lecture 28 Annotation for Anonymous Functions/annotations/functions.ts
@@ -0,0 +1,20 @@
+// Arrow function
+const add = (a: number, b: number): number => {
+  // a-b
+  return a + b;
+};
+
+const subtract = (a: number, b: number) => {
+  // a-b
+  return a - b;
+};
+
+// Function declaration
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+// Function expression or anonymous function
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
