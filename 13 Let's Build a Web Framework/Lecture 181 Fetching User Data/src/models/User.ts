95603a1347e6119f92549191ad41f57ab5529044
MJunaidK <junaidcts@gmail.com>
2023-05-11 22:44:53 -0400

    fetching and saving user data

---
+++ 13 Let's Build a Web Framework/Lecture 181 Fetching User Data/src/models/User.ts
@@ -0,0 +1,56 @@
+import { Attributes } from './Attributes';
+import { Eventing } from './Eventing';
+import { Sync } from './Sync';
+import { AxiosResponse } from 'axios';
+
+export interface UserProps {
+  id?: number;
+  name?: string;
+  age?: number;
+}
+
+const rootUrl = 'http://localhost:3000/users';
+
+export class User {
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
+
+  constructor(private attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
+
+  /*on(eventName: string, callback: Callback): void {
+    this.events.on(eventName, callback);
+  }
+  */
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(update: UserProps) {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.attributes.get('id');
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+}
